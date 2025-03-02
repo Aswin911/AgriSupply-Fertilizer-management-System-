da127e3d035c4f68e7e44b1f46007ba74f126be4
Aswin911 <150098028+Aswin911@users.noreply.github.com>
2025-03-02 09:04:52 +0530

    Initial commit

---
+++ server/index.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+
+app.use(express.json())
+app.use(cors())
+
+const db = require('./models')
+
+const home = require('./routes/Home.js')
+app.use('/',home)
+
+db.sequelize.sync().then(() => {
+    app.listen(3001,()=>{
+        console.log("Server running in port 3001");
+        
+    })
+})
+
